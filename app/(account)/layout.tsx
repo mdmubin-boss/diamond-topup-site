b23ca0c0577c45aa1b078cfc75a4eafe8a497652
mdmubin-boss <mdmubinmiah3@gmail.com>
2026-03-10 15:57:33 +0600

    Add files via upload

--- app/(account)/layout.tsx
+++ app/(account)/layout.tsx
@@ -1,1 +1,2 @@
-
+import BottomNav from "@/components/layout/BottomNav";
+export default function Layout({children}:{children:React.ReactNode}){return <div className="pb-20">{children}<BottomNav/></div>;}
